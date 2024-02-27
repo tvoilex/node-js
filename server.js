39db34a0984da57c377117e4a8c5bcfc92776b4b
tvoilex <m-a-x-99@yandex.by>
2024-02-27 13:09:22 +0100

    asdasd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 2021;
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('AAAA Simple Node.js Server is Up and Running.......')
+    res.send('MAX Simple Node.js Server is Up and Running.......')
 })
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
